be682e10e38f61a5b09888d62dc2033fee31e85e
Igor Alves <igoralves1@gmail.com>
2024-09-29 18:58:05 -0400

    console.log

--- hello/handler.js
+++ hello/handler.js
@@ -1,23 +1,25 @@
 exports.hello = async (event) => {
-  let response;
-  try {
-      const path = event.path;
-      let message = "Hello World!";
-      
-      if (path === "/protected-hello") {
-          message = "Hello from protected endpoint!";
-      }
-      
-      response = {
-          'statusCode': 200,
-          'body': JSON.stringify({
-              message: message,
-          })
-      }
-  } catch (err) {
-      console.log(err);
-      return err;
-  }
+    let response;
+    try {
+        console.log("handler Hello = ", token)
+        console.log("handler Hello event = ", event)
+        const path = event.path;
+        let message = "Hello World!";
+        
+        if (path === "/protected-hello") {
+            message = "Hello from protected endpoint!";
+        }
+        
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: message,
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
 
-  return response
+    return response
 };
